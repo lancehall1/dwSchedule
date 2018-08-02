f65d9f0ea775b681907d5fb35c0db4b90e861608
Lance Hall <Lance@lorenz.com>
2018-08-02 15:13:47 -0400

    updated connection variables

--- php/mysql-credentials.php
+++ php/mysql-credentials.php
@@ -1,6 +1,6 @@
 <?php
-    $servername = "sql13.bravehost.com";
-    $username = "lhall";
-    $password = "Lolipop0";
-    $dbname = "dwDatabase_2820268";
+    $servername = "us-cdbr-iron-east-01.cleardb.net";
+    $username = "bb52cbfcc6260d";
+    $password = "a6836c7f";
+    $dbname = "heroku_01b86a6647f084a";
 ?>
